7bc082f0a375f83175a903ebadd3052b3c2a6aed
AnnisaDwiF <annisadwif92@gmail.com>
2023-03-17 10:03:59 +0700

    create http server

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log(`Hallo, kita akan belajar membuat server`);
+const http = require(`http`);
+
+const requestListener = (request, response) => {
+    response.setHeader(`Content-Type`, `text/html`);
+
+    response.statusCode = 200;
+    response.end(`<h1>Hallo HTTP Server!!`);
+};
+
+const server = http.createServer(requestListener);
+const port = 5000;
+const host = `localhost`;
+
+server.listen(port, host, () => {
+    console.log(`Server berjalan pada http://${host}:${port}`);
+});

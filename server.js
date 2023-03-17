85c6371a3eea93fe32f61b2eeb90a9797dd1f9cd
AnnisaDwiF <annisadwif92@gmail.com>
2023-03-17 10:27:47 +0700

    create method/verb request

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const http = require(`http`);
 
+
+
 const requestListener = (request, response) => {
     response.setHeader(`Content-Type`, `text/html`);
 
     response.statusCode = 200;
-    response.end(`<h1>Hallo HTTP Server!!`);
+
+    const {method} = request;
+
+    if(method == `GET`){
+        response.end(`<h1>Helloo GET method</h1>`);
+    }
+    if(method == `POST`){
+        response.end(`<h1>Hi POST method</h1>`);
+    }
+    if(method == `PUT`){
+        response.end(`<h1>Helloo PUT method</h1>`);
+    }
+    if(method == `DELETE`){
+        response.end(`<h1>Hi DELETE method</h1>`);
+    }
 };
 
 const server = http.createServer(requestListener);
